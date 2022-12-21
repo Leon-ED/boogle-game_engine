9d6780cf4b6019aeb5fea5736b9698d8f0826d1f
Leon-ED <edmeeleon@outlook.fr>
2022-12-21 11:32:02 +0100

    Construction du CSTree depuis un fichier (enfin)

--- server/dictionnary/dictionnary_build.c
+++ server/dictionnary/dictionnary_build.c
@@ -1,25 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/dictionnary_build.h"
 
-#include "include/dictionnary.h"
-#include "include/trees.h"
-#include "string.h"
-
-/** ===== DICTIONNARY BUILD ===== **/
 
 /**
- * @brief      Construit le dictionnaire
- * @param      input_name   Le nom du fichier d'entrée
- * @param      output_name  Le nom du fichier de sortie
- */
-void dictionnary_build(char input_name[], char output_name[])
+ * @brief Insère un mot dans l'arbre dans l'ordre alphabétique
+ * @param t Arbre dans lequel insérer le mot
+ * @param word Mot à insérer
+ * @return void
+ **/
+void insertWord(CSTree *t, char *word)
 {
-    // On ouvre les fichiers et on teste leur ouverture
-    FILE *input_FILE = fopen(input_name, "r");
-    FILE *output_FILE = fopen(output_name, "w");
-
-    // On récupère le nombre de lignes du fichier d'entrée
-    int nb_lines = number_of_lines(input_FILE);
+  printf("word: %s ", word);
+  if (*t == NULL)
+  {
+    *t = newTree(word[0], NULL, NULL);
+    if (word[1] != '\0')
+      insertWord(&(*t)->firstChild, word + 1);
+  }
+  else if ((*t)->elem == word[0])
+  {
+    if (word[1] != '\0')
+      insertWord(&(*t)->firstChild, word + 1);
+  }
+  else if ((*t)->elem > word[0])
+  {
+    // printf("AU DESSUS");
+    // printf("word: %c, char %c ", word[0], (*t)->elem);
+    CSTree new = newTree(word[0], NULL, *t);
+    *t = new;
+    if (word[1] != '\0')
+      insertWord(&(*t)->firstChild, word + 1);
+  }
+  else
+  {
+    insertWord(&(*t)->nextSibling, word);
+  }
 }
 
-// int main(int argc, char *argv[]){
-//     return 0;
-// }
+
+/**
+ * @brief Construit le dictionnaire à partir du fichier dico.txt
+ * @param dico_path Chemin vers le fichier dico.txt où se trouve le dictionnaire
+ * @param dico_lex_path Chemin vers le fichier dico_lexe.lex où sera stocké le dictionnaire lexé
+ * @return void
+ */
+void dictionnary_build(char *dico_path, char *dico_lex_path)
+{
+  FILE *dico = fopen(dico_path, "r");
+  FILE *dico_lex = fopen(dico_lex_path, "wb");
+  FileHeader header;
+  header.nb_words = number_of_lines(dico);
+  printf("nb_words: %d \n", header.nb_words);
+  if (dico == NULL || dico_lex == NULL)
+  {
+    printf("Erreur : Impossible de trouver l'un des deux fichiers, etes-vous sur de leur emplacement ? !\n");
+    exit(1);
+  }
+  printf("nb_words: %d \n", header.nb_words);
+
+  char line[256];
+  int i = 0;
+  CSTree tree = NULL;
+  while (fgets(line, sizeof(line), dico))
+  {
+    insertWord(&tree, line);
+    i++;
+  }
+  printPrefix(tree);
+  fclose(dico);
+}
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+  dictionnary_build("../files/dico.txt", "../files/dumps/dico_lexe.lex");
+  return 0;
+}
---
+++ server/dictionnary/include/dictionnary_build.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include "libs.h"
+#include "trees.h"
+#include "string.h"
+
+
+// void dictionnary_build(char input_name[], char output_name[]);
+void dictionnary_build(char *dico_path, char *dico_lex_path);
