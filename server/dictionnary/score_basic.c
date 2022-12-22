2381f0f881fd75b119c63a8ff78d3132fdd9db33
Leon-ED <edmeeleon@outlook.fr>
2022-12-22 15:43:41 +0100

    Calcul basique du score (1pt par mot) plus tard le calcul se fera selon la frequence  des lettres

---
+++ server/dictionnary/score_basic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * @brief Retourne simplement 1 point par mot passé en argument
+ **/
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    if(argc <= 1){
+        printf("SCORE : Merci de fournir au moins un mot en argument\n");
+        return 1;
+    }
+    printf("%d\n", argc-1);
+    
+}
