99ed61c2c44008d8cff7e280e61c8ea58f10d330
Leon-ED <edmeeleon@outlook.fr>
2023-01-25 23:35:36 +0100

    modif .gitignore

--- server/dictionnary/src/include/solve.h
+++ server/dictionnary/src/include/solve.h
@@ -1,20 +1,14 @@
-#include "grid_path.h"
-#include "dictionnary_lookup.h"
+
 
 // Définition de la structure de données représentant la grille de caractères
 // Structure for the grid of characters
-// Structure for the grid of characters
-typedef struct
-{
-    int rows;
-    int cols;
-    char *letters;
-} Grid;
+// // Structure for the grid of characters
+// typedef struct
+// {
+//     int rows;
+//     int cols;
+//     char *letters;
+// } Grid;
 
 // Linked list node structure
-typedef struct list List;
-struct list
-{
-    List *next;
-    void *data;
-};
+
